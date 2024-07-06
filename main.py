355ed98bfcd94509090414e68e6401c14ca3f693
Aubrey Malabie <aubrey@aftarobot.com>
2024-07-06 12:53:36 +0200

    🍎🍎 initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, my fucking name is bloody {name} !!')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(' 🍎  🍎  🍎 PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
